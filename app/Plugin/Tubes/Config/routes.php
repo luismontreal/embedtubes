7ab7e7c55a4d5e5c244f7ea1255901a451d88219
Luigi <luismontreal@gmail.com>
2013-06-17 22:27:34 -0400

    changed home route to be for my plugin

--- app/Plugin/Tubes/Config/routes.php
+++ app/Plugin/Tubes/Config/routes.php
@@ -1,3 +1,12 @@
 <?php
+// Basic
+Router::promote(
+        count(
+                CroogoRouter::connect('/', array(
+                    'plugin' => 'nodes', 'controller' => 'nodes', 'action' => 'index', 'type' => 'video'
+                    )
+                )
+        ) -1
+);
 
 CroogoRouter::contentType('video');
